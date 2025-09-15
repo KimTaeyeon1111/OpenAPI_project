281dc0b9786c8e02c20912964107dd1556156c8d
KimTaeyeon1111 <rlaxodus1001@naver.com>
2025-09-15 16:45:42 +0900

    20250905

--- js/book.js
+++ js/book.js
@@ -1,97 +1,173 @@
 // Most popular book 파트
-const cards = document.querySelectorAll(".sec1");
+const cards = document.querySelectorAll('.tab-menu button');
+console.log(cards);
 
-        cards.forEach(card => {
-            card.addEventListener("click", () => {
-            // 모든 카드에서 active 제거
-            cards.forEach(c => c.classList.remove("active"));
+cards.forEach(card => {
+    card.addEventListener("click", () => {
+    // 모든 카드에서 active 제거
+    cards.forEach(c => c.classList.remove("active"));
 
-            // 클릭한 카드만 active 추가
-            card.classList.add("active");
-            });
-        });
-
-     
-        const tabMenu = document.querySelectorAll('.tab-menu button');
-        const tabContent = document.querySelectorAll('#tab-content > div');
-        tabMenu.forEach(function(item, idx){
-            item.addEventListener('click', function(e){
-                e.preventDefault();
-                showContent(idx);
-            });
-        });
-        function showContent(num){
-            tabContent.forEach(function(item){
-                item.style.display = 'none';
-            });
-            tabContent[num].style.display = 'block';
-        };
-
-
-
-
-
-
-
-async function fetchBooks(query) {
-    const REST_API_KEY = '1253f204f41b8b59c6bb69854384fc046'
-    const params = new URLSearchParams({
-        target: "title",
-        query,
-        size: 50
+    // 클릭한 카드만 active 추가
+    card.classList.add("active");
     });
-    const url = `https://dapi.kakao.com/v3/search/book?${params}`;
+});
 
-    const response = await fetch(url, {
-        method: 'GET',
-        headers: {
-            Authorization: `KakaoAK ${REST_API_KEY}`
-        }
+
+const tabMenu = document.querySelectorAll('.tab-menu div');
+const tabContent = document.querySelectorAll('#tab-content > div');
+tabMenu.forEach(function(item, idx){
+    item.addEventListener('click', function(e){
+        e.preventDefault();
+        showContent(idx);
     });
+});
+function showContent(num){
+    tabContent.forEach(function(item){
+        item.style.display = 'none';
+    });
+    tabContent[num].style.display = 'block';
+};
 
-    if (!response.ok) {
-        throw new Error(`HTTP 오류: ${response.status}`);
-    }
 
-    return response.json();
-}
+
+//--------------------------------------------------------------------------
+// API
+//--------------------------------------------------------------------------
+
+src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"
 
 async function bookData() {
-            try {
-                const querys = ["자바스크립트", "강아지"];
+    const params = new URLSearchParams({
+        target: "title",
+        query: "강아지"
+    });
 
-                for (const q of querys) {
-                    const data = await fetchBooks(q);
-                    console.log(data);
-                }
-                // .box 요소 전체 선택
-                const boxElements = document.querySelectorAll(".box");
-                console.log(boxElements.length)
-
-                // documents 데이터를 각 box에 대응하여 렌더링
-                for(let i=0; i<boxElements.length; i++){
-                    const doc = data.documents[i];
-                    const box = boxElements[i];
-
-                    // <img>
-                    const img = document.createElement("img");
-                    img.src = doc.thumbnail;
-                    box.appendChild(img);
-
-                    // <h3> 제목
-                    const h3 = document.createElement("h3");
-                    h3.textContent = doc.title;
-                    box.appendChild(h3);
-
-            // 별 넣기
-                    const p = document.createElement("p");
-                    p.textContent = doc.contents.substring(0, 60);
-                    box.appendChild(p);
-                };
-
-            } catch (error) {
-                console.log('에러발생', error);
+    try {
+        const response = await fetch(`https://dapi.kakao.com/v3/search/book?${params}`, {
+            method: 'GET',
+            headers: {
+                Authorization: "KakaoAK 253f204f41b8b59c6bb69854384fc046"
             }
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP 오류! 상태 코드: ${response.status}`);
         }
 
-        bookData();
+        const data = await response.json();
+
+        // .box 요소 전체 선택
+        const boxElements = document.querySelectorAll(".mySwiper10 .box");
+
+        // documents 데이터를 각 box에 대응하여 렌더링
+        for(let i=0; i<boxElements.length; i++){
+            const doc = data.documents[i];
+            const box = boxElements[i];
+
+            // <img>
+            const img = document.createElement("img");
+            img.src = doc.thumbnail;
+            box.appendChild(img);
+
+            // <h3> 제목
+            const h3 = document.createElement("h3");
+            h3.textContent = doc.title;
+            box.appendChild(h3);
+
+            // <h6> 저자
+            const h6 = document.createElement("h6");
+            h6.textContent = doc.authors;
+            box.appendChild(h6);
+
+            // <p> 내용 일부
+            const p = document.createElement("p");
+            p.textContent = doc.contents.substring(0, 45)+"";
+            box.appendChild(p);
+
+            const star = document.createElement("p");
+            const rating = Math.floor(Math.random() * 5) + 1;
+            const filledStars = "★".repeat(rating);
+            const emptyStars = "☆".repeat(5 - rating);
+            star.textContent = filledStars + emptyStars;
+            star.style.color = "gold";
+            star.style.fontSize = "1.2em";
+            box.appendChild(star);
+
+        };
+
+    } catch (error) {
+        console.log('에러발생', error);
+    }
+}
+
+        
+async function bookData2() {
+    const params = new URLSearchParams({
+        target: "title",
+        query: "여행을 떠나요"
+    });
+
+    try {
+        const response = await fetch(`https://dapi.kakao.com/v3/search/book?${params}`, {
+            method: 'GET',
+            headers: {
+                Authorization: "KakaoAK 253f204f41b8b59c6bb69854384fc046"
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP 오류! 상태 코드: ${response.status}`);
+        }
+
+        const data = await response.json();
+
+        // .box 요소 전체 선택
+        const boxElements = document.querySelectorAll(".mySwiper11 .box");
+
+        // documents 데이터를 각 box에 대응하여 렌더링
+        for(let i=0; i<boxElements.length; i++){
+            const doc = data.documents[i];
+            const box = boxElements[i];
+
+            // <img>
+            const img = document.createElement("img");
+            img.src = doc.thumbnail;
+            box.appendChild(img);
+
+            // <h3> 제목
+            const h3 = document.createElement("h3")
+            h3.textContent = doc.title.substring(0, 8);
+            box.appendChild(h3);
+
+            // <h6> 저자
+            const h6 = document.createElement("h6");
+            h6.textContent = doc.authors;
+            box.appendChild(h6);
+
+            // <p> 내용 일부
+            const p = document.createElement("p");
+            p.textContent = doc.contents.substring(0, 45)+"";
+            box.appendChild(p);
+
+            const star = document.createElement("p");
+            const rating = Math.floor(Math.random() * 5) + 1;
+            const filledStars = "★".repeat(rating);
+            const emptyStars = "☆".repeat(5 - rating);
+            star.textContent = filledStars + emptyStars;
+            star.style.color = "gold";
+            star.style.fontSize = "1.2em";
+            box.appendChild(star);
+
+        };
+
+    } catch (error) {
+        console.log('에러발생', error);
+    }
+}
+
+bookData();
+bookData2();
+
+
+
+    
