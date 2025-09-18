1ef774f1114cda32a558ede6a13bb03e53cdf631
KimTaeyeon1111 <rlaxodus1001@naver.com>
2025-09-18 17:56:08 +0900

    20250918

--- js/sub_page_book.js
+++ js/sub_page_book.js
@@ -11,6 +11,7 @@ src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"
         nextEl: ".swiper-button-next3",
         prevEl: ".swiper-button-prev3",
       },
+      allowTouchMove: false,
     });
 
     var swiper2 = new Swiper(".mySwiper2", {
@@ -24,13 +25,14 @@ src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"
       allowTouchMove: false,
     });
 
-    
+//-------------------------------------------------------------------------
 //첫 번째 슬라이드 API
+//-------------------------------------------------------------------------
 src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"
 
 async function bookData() {
     const params = new URLSearchParams({
-        query: "안녕"
+        query: "The lord"
     });
 
     try {
@@ -46,27 +48,27 @@ async function bookData() {
         }
 
     // 영어가 많이 있는 자료만 뽑아서 출력하기
-    /*
+    const Maindata = await response.json();
     const origin = Maindata.documents;
     function isMostlyEnglish(text, threshold = 0.7) {
         const englishChars = text.match(/[a-zA-Z]/g) || [];
         const totalChars = text.replace(/\s/g, '');
         return totalChars.length > 0 && (englishChars.length / totalChars.length) >= threshold;
 
-    } 
+    }
     const data = origin.filter((val)=>{
                     return val.contents && isMostlyEnglish(val.contents);
-    })*/
-        const data = await response.json();
+    })  
+
         // .box 요소 전체 선택
         
-        const boxElements = document.querySelectorAll(".mySwiper .swiper-slide >div");
+        const boxElements = document.querySelectorAll(".mySwiper .swiper-slide > button");
         const boxElements2 = document.querySelectorAll(".mySwiper2 .swiper-slide>div");
-        const boxtext = document.querySelectorAll(".sub-book-2 > div");
+        const boxtext = document.querySelectorAll(".tab-content > div");
         console.log(boxtext)
         // documents 데이터를 각 box에 대응하여 렌더링
         for(let i=0; i<boxElements.length; i++){
-            const doc = data.documents[i];
+            const doc = data[i];
             const box = boxElements[i];
             const box2 = boxElements2[i];
             const text = boxtext[i];
@@ -86,6 +88,12 @@ async function bookData() {
             h3.textContent = doc.title
             text.appendChild(h3);
 
+            // <h6> 저자
+            const h6 = document.createElement("h6");
+            h6.textContent = doc.price;
+            h6.style.color = '#25a925'
+            text.appendChild(h6);
+
             const star = document.createElement("p");
             const rating = Math.floor(Math.random() * 5) + 1;
             const filledStars = "★".repeat(rating);
@@ -93,12 +101,8 @@ async function bookData() {
             star.textContent = filledStars + emptyStars;
             star.style.color = "gold";
             star.style.fontSize = "1.2em";
+            star.style.padding = "0 0 15px 0";
             text.appendChild(star);
-
-            // <h6> 저자
-            const h6 = document.createElement("h6");
-            h6.textContent = doc.authors;
-            text.appendChild(h6);
 
             // <p> 내용 일부
             const p = document.createElement("p");
@@ -115,3 +119,35 @@ async function bookData() {
 }
 
 bookData();
+
+//-------------------------------------------------------------------------
+// 탭 메뉴 //
+//-------------------------------------------------------------------------
+
+const buttons = document.querySelectorAll('.mySwiper .swiper-wrapper > div');
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+    // 모든 카드에서 active 제거
+    buttons.forEach(btn => btn.classList.remove("active"));
+
+    // 클릭한 카드만 active 추가
+    button.classList.add("active");
+    });
+});
+
+
+const tabMenu = document.querySelectorAll('.swiper-slide button');
+const tabContent = document.querySelectorAll('.tab-content > div');
+tabMenu.forEach(function(item, idx){
+    item.addEventListener('click', function(e){
+        e.preventDefault();
+        showContent(idx);
+    });
+});
+function showContent(num){
+    tabContent.forEach(function(item){
+        item.style.display = 'none';
+    });
+    tabContent[num].style.display = 'block';
+};
